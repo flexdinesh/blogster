908325fd4d9a126a200b447b14b7d1861f9fcc27
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-12-03 22:39:53 +1100

    monorepo things

--- packages/@repoconfig/eslint-config-custom/index.js
+++ packages/@repoconfig/eslint-config-custom/index.js
@@ -1,7 +1,31 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
+  extends: ["turbo", "prettier", "plugin:astro/recommended"],
+  // parserOptions: {
+  //   ecmaVersion: "latest",
+  //   sourceType: "module",
+  // },
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      rules: {
+      },
+    }    
+  ],
 };
