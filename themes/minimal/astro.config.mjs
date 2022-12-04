6409f20021e1b4012994c37d6cb9c3d9176cd03b
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-12-04 11:40:34 +1100

    switch server start ports

--- themes/minimal/astro.config.mjs
+++ themes/minimal/astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // TODO:
-let BASE_URL = "http://localhost:3000";
+let BASE_URL = "http://localhost:9000";
 
 export default defineConfig({
+  server: { port: 9000 },
   site: BASE_URL,
   integrations: [sitemap()],
 });
--- themes/sleek/astro.config.mjs
+++ themes/sleek/astro.config.mjs
@@ -3,9 +3,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // TODO:
-let BASE_URL = "http://localhost:3000";
+let BASE_URL = "http://localhost:9001";
 
 export default defineConfig({
+  server: { port: 9001 },
   site: BASE_URL,
   integrations: [tailwind(), sitemap()],
 });
