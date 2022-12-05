8b833772da72ba1b8f076f9cbb8ade369b8bbd2b
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-12-05 21:17:43 +1100

    debug minimal

--- themes/minimal/astro.config.mjs
+++ themes/minimal/astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
+console.log({ isBuild, URL: process.env.URL });
 
 let BASE_URL = "http://localhost:9000";
 
