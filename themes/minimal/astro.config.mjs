9d3f059d944aa565cf0b18f4d6f48aa640cbcc8d
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-12-05 21:24:49 +1100

    SITE_URL in build finally works

--- themes/minimal/astro.config.mjs
+++ themes/minimal/astro.config.mjs
@@ -6,17 +6,9 @@ import sitemap from "@astrojs/sitemap";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-console.log({ isBuild, URL: process.env.URL });
-
 let BASE_URL = "http://localhost:9000";
-
-// If you're using Netlify, your deploy URL will be available as
-// an environment variable URL during build.
-if (isBuild && process.env.URL) {
-  BASE_URL = process.env.URL;
-}
-// If you're not Netlify, you could just set your URL manually
-if (isBuild && !process.env.URL) {
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
   BASE_URL = "https://blogster-minimal.netlify.app";
 }
 
--- themes/sleek/astro.config.mjs
+++ themes/sleek/astro.config.mjs
@@ -7,17 +7,9 @@ import tailwind from "@astrojs/tailwind";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-console.log({ isBuild, URL: process.env.URL });
-
 let BASE_URL = "http://localhost:9001";
-
-// If you're using Netlify, your deploy URL will be available as
-// an environment variable URL during build.
-if (isBuild && process.env.URL) {
-  BASE_URL = process.env.URL;
-}
-// If you're not Netlify, you could just set your URL manually
-if (isBuild && !process.env.URL) {
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
   BASE_URL = "https://blogster-sleek.netlify.app";
 }
 
