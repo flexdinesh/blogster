8e639bd2e645d18c9bc97c513e3158badc9d5c97
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-12-05 21:07:57 +1100

    debug build

--- themes/sleek/astro.config.mjs
+++ themes/sleek/astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
+console.log({ isBuild, URL: process.env.URL });
 
 let BASE_URL = "http://localhost:9001";
 
