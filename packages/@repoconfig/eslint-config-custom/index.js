1f1966e66d3dff2b3a9d389478824f3ce166ebc7
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-12-10 11:31:11 +1100

    repo lint config changes

--- packages/@repoconfig/eslint-config-custom/index.js
+++ packages/@repoconfig/eslint-config-custom/index.js
@@ -1,5 +1,18 @@
 module.exports = {
-  extends: ["turbo", "prettier"],
+  extends: [
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   // don't lint astro themes
-  ignorePatterns: ["themes/**/*"],
+  // don't lint cli package (it has its own lint config)
+  ignorePatterns: [
+    "themes/**/*",
+    ".eslintrc.js",
+    "packages/cli",
+    "packages/@repoconfig",
+  ],
 };
