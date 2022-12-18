817a2def8bdbc5969e08fa82ec1b2082666d8569
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-12-18 22:59:44 +1100

    (cli) sync latest templates

---
+++ packages/create-blogster/templates/bubblegum/src/lib/markdoc/config.ts
@@ -0,0 +1,98 @@
+import Markdoc from "@markdoc/markdoc";
+import type { Config } from "@markdoc/markdoc";
+
+const { nodes, Tag } = Markdoc;
+
+/* Markdoc config goes here. https://markdoc.dev/docs/config */
+export const config: Config = {
+  tags: {
+    details: {
+      render: "details",
+      children: nodes.document.children,
+    },
+    summary: {
+      render: "summary",
+      children: nodes.document.children,
+    },
+    sup: {
+      render: "sup",
+      children: nodes.strong.children,
+    },
+    sub: {
+      render: "sub",
+      children: nodes.strong.children,
+    },
+    abbr: {
+      render: "abbr",
+      attributes: {
+        title: { type: String },
+      },
+      children: nodes.strong.children,
+    },
+    kbd: {
+      render: "kbd",
+      children: nodes.strong.children,
+    },
+    mark: {
+      render: "mark",
+      children: nodes.strong.children,
+    },
+    youtube: {
+      render: "YouTubeEmbed",
+      attributes: {
+        url: { type: String, required: true },
+        label: { type: String, required: true },
+      },
+      selfClosing: true,
+    },
+  },
+  nodes: {
+    heading: {
+      render: "Heading",
+      attributes: {
+        level: { type: Number, required: true },
+      },
+      transform(node, config) {
+        const attributes = node.transformAttributes(config);
+        const children = node.transformChildren(config);
+        return new Tag(this.render, { ...attributes }, children);
+      },
+    },
+    // if you want to customise default tags, this is where you'd do it
+    // after adding the code here, add an Astro component for this node
+    // in MarkdownRenderer.astro component
+    // paragraph: {
+    //   render: "paragraph",
+    //   transform(node, config) {
+    //     const attributes = node.transformAttributes(config);
+    //     const children = node.transformChildren(config);
+    //     return new Tag(this.render, { ...attributes }, children);
+    //   },
+    // },
+    fence: {
+      render: "CodeBlock",
+      attributes: {
+        content: { type: String, render: false, required: true },
+        language: { type: String, default: "typescript" },
+        // process determines whether or not markdoc processes tags inside the content of the code block
+        process: { type: Boolean, render: false, default: false },
+      },
+      transform(node, config) {
+        const attributes = node.transformAttributes(config);
+        const children = node.transformChildren(config);
+        if (children.some((child) => typeof child !== "string")) {
+          throw new Error(
+            `unexpected non-string child of code block from ${
+              node.location?.file ?? "(unknown file)"
+            }:${node.location?.start.line ?? "(unknown line)"}`
+          );
+        }
+        return new Tag(
+          this.render,
+          { ...attributes, content: children.join("") },
+          []
+        );
+      },
+    },
+  },
+};
---
+++ packages/create-blogster/templates/bubblegum/src/lib/markdoc/read.ts
@@ -0,0 +1,131 @@
+import path from "path";
+import { globby } from "globby";
+import type { RenderableTreeNode } from "@markdoc/markdoc";
+import { parseAndTransform } from "./transform";
+import { validateBlogFrontmatter } from "./blog/frontmatter";
+import { validateProjectFrontmatter } from "./project/frontmatter";
+
+// path is relative to where you run the `yarn build` command
+const pathToContentDir = path.normalize(
+  "./content"
+);
+
+type Content = {
+  path: string;
+  slug: string;
+  content: RenderableTreeNode;
+  frontmatter: Record<string, unknown>;
+};
+
+type ReturnTypeForBlog = Content & {
+  frontmatter: ReturnType<typeof validateBlogFrontmatter>;
+};
+type ReturnTypeForProjects = Content & {
+  frontmatter: ReturnType<typeof validateProjectFrontmatter>;
+};
+
+/* 
+  If you are adding a new content directory like "notes" or "talk",
+  you need to add a function overload for readAll and readOne.
+*/
+/* Overloads for readAll - start */
+export async function readAll(args: {
+  directory: "blog";
+}): Promise<ReturnTypeForBlog[]>;
+export async function readAll(args: {
+  directory: "projects";
+}): Promise<ReturnTypeForProjects[]>;
+/* Overloads for readAll - end */
+
+export async function readAll({
+  directory,
+}: {
+  directory: string;
+}): Promise<Content[]> {
+  const pathToDir = path.join(pathToContentDir, directory);
+  const paths = await globby(`${pathToDir}/*.md`);
+
+  const files = await Promise.all(
+    paths.map((path) => parseAndTransform({ path }))
+  );
+
+  if (directory === "blog") {
+    const content = files.map((file) => {
+      return {
+        ...file,
+        frontmatter: validateBlogFrontmatter(file.frontmatter),
+      };
+    });
+    return content;
+  }
+
+  if (directory === "projects") {
+    const content = files.map((file) => {
+      return {
+        ...file,
+        frontmatter: validateProjectFrontmatter(file.frontmatter),
+      };
+    });
+    return content;
+  }
+
+  throw new Error(
+    "type should be one of the available types in ContentDirectory. If you are adding a new directory in Content, then please make sure you include it in ContentType for strong frontmatter type."
+  );
+}
+
+/* 
+  If you are adding a new content directory like "notes" or "talk",
+  you need to add a function overload for readAll and readOne.
+*/
+/* Overloads for readOne - start */
+export async function readOne(args: {
+  directory: "blog";
+  filename: string;
+}): Promise<ReturnTypeForBlog>;
+export async function readOne(args: {
+  directory: "projects";
+  filename: string;
+}): Promise<ReturnTypeForProjects>;
+/* Overloads for readOne - end */
+
+export async function readOne({
+  directory,
+  filename,
+}: {
+  directory: string;
+  filename: string;
+}) {
+  const pathToDir = path.join(pathToContentDir, directory);
+  const absolutePath = path.join(pathToDir, filename);
+
+  const {
+    content,
+    frontmatter,
+    path: filepath,
+    slug,
+  } = await parseAndTransform({
+    path: absolutePath,
+  });
+
+  if (directory === "blog") {
+    return {
+      content,
+      frontmatter: validateBlogFrontmatter(frontmatter),
+      path: filepath,
+      slug,
+    };
+  }
+  if (directory === "projects") {
+    return {
+      content,
+      frontmatter: validateProjectFrontmatter(frontmatter),
+      path: filepath,
+      slug,
+    };
+  }
+
+  throw new Error(
+    "type should be one of the available types in ContentType. If you are adding a new directory in Content, then please make sure you include it in ContentType for stronger frontmatter type."
+  );
+}
